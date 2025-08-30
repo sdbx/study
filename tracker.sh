9d49ba17b115a0eb01dc548d0cd522ca65a143f8
tnraro <deokji53@gmail.com>
2025-08-30 16:28:07 +0900

    feat: improve tracker

--- tracker.sh
+++ tracker.sh
@@ -18,6 +18,9 @@ else
    fi
 fi
 
+# Auto-detect users from existing .md files
+USERS=($(find "$dirname" -name "*.md" -not -name "README.md" | xargs -n1 basename | sed 's/\.md$//' | sort -u))
+
 # Colors
 dirc="\033[36m"
 goodc="\033[38;5;49;1m"
@@ -32,65 +35,66 @@ echo "###########"
 echo
 
 all_count=0
-raro_count=0
-cor_count=0
+declare -A user_counts
+declare -A user_flags
+
+# Initialize user counts
+for user in "${USERS[@]}"; do
+   user_counts[$user]=0
+done
+
 cd $dirname
 dirs=$(ls -d $path)  # Lists only directories
 for dir in $dirs; do
    all_count=$(($all_count+1))
-   raro_flag="${badc}WIP${init}"
-   cor_flag="${badc}WIP${init}"
-
-   if [ -e $dir/raro.md ]  # Is there a file?
-   then
-      raro_count=$(($raro_count+1))
-      raro_flag="${goodc}DONE!${init}"
-   fi
    
-   if [ -e $dir/cor.md ]
-   then
-      cor_count=$(($cor_count+1))
-      cor_flag="${goodc}DONE!${init}"
-   fi
+   # Check each user's file and set flags
+   for user in "${USERS[@]}"; do
+      user_flags[$user]="${badc}WIP${init}"
+      if [ -e $dir/$user.md ]; then
+         user_counts[$user]=$((${user_counts[$user]}+1))
+         user_flags[$user]="${goodc}DONE!${init}"
+      fi
+   done
 
    _dir=$(echo $dir | sed 's/.$//')  # Deletes the last character
-   printf "$dirc$_dir$init\n  RARO $raro_flag\tCOR $cor_flag"
+   printf "$dirc$_dir$init\n"
+   for user in "${USERS[@]}"; do
+      user_upper=$(echo $user | tr '[:lower:]' '[:upper:]')
+      printf "  $user_upper ${user_flags[$user]}\t"
+   done
    echo
 done
 
-# fixme: use function to remove redundancy
 echo
 printf "${progc}PROGRESS${init}"
 echo
-raro_perct=$(($raro_count*100/$all_count))
-cor_perct=$(($cor_count*100/$all_count))
-raro_nsharp=$(($raro_perct/10))
-cor_nsharp=$(($cor_perct/10))
-raro_ndot=$((10-$raro_nsharp))
-cor_ndot=$((10-$cor_nsharp))
-raro_resstr=""
-cor_resstr=""
-count=0
-while [ $count -lt $raro_nsharp ]; do
-   raro_resstr="${raro_resstr}#"
-   count=$(($count+1))
+
+# Function to generate progress bar
+generate_progress_bar() {
+   local percentage=$1
+   local nsharp=$(($percentage/10))
+   local ndot=$((10-$nsharp))
+   local resstr=""
+   local count=0
+
+   while [ $count -lt $nsharp ]; do
+      resstr="${resstr}#"
+      count=$(($count+1))
+   done
+   count=0
+   while [ $count -lt $ndot ]; do
+      resstr="${resstr}."
+      count=$(($count+1))
+   done
+   echo "$resstr"
+}
+
+# Display progress for each user
+for user in "${USERS[@]}"; do
+   user_perct=$((${user_counts[$user]}*100/$all_count))
+   user_upper=$(echo $user | tr '[:lower:]' '[:upper:]')
+   progress_bar=$(generate_progress_bar $user_perct)
+   printf "%-4s %3i%% [%s]" "$user_upper" $user_perct "$progress_bar"
+   echo
 done
-count=0
-while [ $count -lt $raro_ndot ]; do
-   raro_resstr="${raro_resstr}."
-   count=$(($count+1))
-done
-count=0
-while [ $count -lt $cor_nsharp ]; do
-   cor_resstr="${cor_resstr}#"
-   count=$(($count+1))
-done
-count=0
-while [ $count -lt $cor_ndot ]; do
-   cor_resstr="${cor_resstr}."
-   count=$(($count+1))
-done
-printf "RARO %3i%% [$raro_resstr]" $raro_perct
-echo
-printf "COR  %3i%% [$cor_resstr]" $cor_perct
-echo
